d6e147d52fcf8f041f60b875b679d0e2c55f9afb
Tyago Nunes <tyagonunes@gmail.com>
2022-03-02 20:41:01 -0300

    fix test

--- setup/tests.py
+++ setup/tests.py
@@ -20,20 +20,18 @@ class AnimaisTestCase(LiveServerTestCase):
 
 
     def test_buscando_um_novo_animal(self):
-        """
-        Teste se um usuário encontra um animal na pesquisa
-        """
+        """Teste se um usuário encontra um animal na pesquisa"""
         home_page = self.browser.get(self.live_server_url + '/')
         brand_element = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         self.assertEqual('Busca Animal', brand_element.text)
 
         buscar_animal_input = self.browser.find_element_by_css_selector('input#buscar-animal')
-        self.assertEqual(buscar_animal_input.get_attribute('placeholder'), 'Exemplo: leão')
+        self.assertEqual(buscar_animal_input.get_attribute('placeholder'), 'Exemplo: leão, urso...')
 
         buscar_animal_input.send_keys('leão')
         time.sleep(2)
         self.browser.find_element_by_css_selector('form button').click()
 
 
-        caracteristicas = self.browser.find_elements_by_css_selector('.results-description')
+        caracteristicas = self.browser.find_elements_by_css_selector('.result-description')
         self.assertGreater(len(caracteristicas), 3)
